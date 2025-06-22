675e313135cc986aa56bcdb7855a05ea9ba4667b
netshield-admin <github@netshield.ltd>
2025-06-22 15:26:22 +0300

    Create install.sh

---
+++ 3x-ui/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# metadata_begin
+# recipe: 3X-UI
+# tags: debian11,debian12,ubuntu2204,ubuntu2404
+# revision: 1
+# description_ru: Рецепт установки 3X-UI (доступы от панели управления находятся в /root/3x-ui.txt)
+# description_en: 3X-UI installation recipe (control panel accesses are in /root/3x-ui.txt)
+# metadata_end
+#
+
+RNAME="3x-ui"
+
+set -x
+
+LOG_PIPE=/tmp/log.pipe.$$                                                                                                                                                                                                                    
+mkfifo ${LOG_PIPE}
+LOG_FILE=/tmp/${RNAME}_command.log
+touch ${LOG_FILE}
+chmod 600 ${LOG_FILE}
+
+tee < ${LOG_PIPE} ${LOG_FILE} &
+
+exec > ${LOG_PIPE}
+exec 2> ${LOG_PIPE}
+
+killjobs() {
+	jops="$(jobs -p)"
+	test -n "${jops}" && kill ${jops} || :
+}
+trap killjobs INT TERM EXIT
+
+echo
+echo "=== Recipe ${RNAME} started at $(date) ==="
+echo
+
+apt update -y
+apt install tmux curl nginx -y
