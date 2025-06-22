3e0d052e4b93e983cfb5f0b4bda3d8c2aa230c6d
netshield-admin <github@netshield.ltd>
2025-06-22 15:59:39 +0300

    Update install.sh

--- 3x-ui/install.sh
+++ 3x-ui/install.sh
@@ -38,4 +38,4 @@ echo
 apt update -y
 apt install curl wget -y
 
-bash <(curl -Ls https://raw.githubusercontent.com/netshield-uk/vm6-repo/refs/heads/main/3x-ui/main.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/netshield-uk/vm6-repo/refs/heads/main/3x-ui/main.sh > /tmp/3x-ui_install.log)
